9616d177e77bff4f10de79379465351fa5548af0
Sayem995 <sayem.bokhari.1@gmail.com>
2020-02-24 19:16:21 +0000

    third failing test passing

--- lib/school_report.rb
+++ lib/school_report.rb
@@ -2,8 +2,10 @@ class SchoolReport
   def format(grades)
     if grades == "Green"
       "Green: 1"
-    else grades == "Green, Green"
+    elsif grades == "Green, Green"
       "Green: 2"
+    else grades == "Green, Amber"
+      "Green: 1, Amber: 1"
     end
   end
 end
